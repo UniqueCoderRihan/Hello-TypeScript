daf124f2db7d7176d897721c6607b40c170e493f
uniquecoderrihan <rihanthedev@gmail.com>
2023-11-23 22:22:39 +0600

    str

--- output/start.js
+++ output/start.js
@@ -1,4 +1,6 @@
 "use strict";
 console.log('this is from startTs');
 const nums = [12, 12, 11, 14, 11, 133];
-
+console.log(country);
+let name = "hi";
+let h = 7;
--- src/start.ts
+++ src/start.ts
@@ -1,3 +1,5 @@
 console.log('this is from startTs')
 const nums =[12,12,11,14,11,133];
 console.log(country);
+let name ="hi";
+let h =7;
